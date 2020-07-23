1d1f8bf1c82277944c9be18929736923e7f52ac1
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-23 13:16:39 -0500

    added MuiThemeProvider to index.js, wrapping App

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from '@material-ui-core/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
