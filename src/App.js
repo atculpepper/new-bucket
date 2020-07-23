48c68219e8261a8dbc9293ee1344f78e34dd3221
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-23 13:52:06 -0500

    deleted old unused components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   return (
     <div className={classes.root}>
       <Header color='primary' />
-      {/* <header className="App-header"> */}
       <img src={logo} className='App-logo' alt='logo' />
     </div>
   );
