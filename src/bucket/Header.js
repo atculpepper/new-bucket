98c8acfb078714636db8ba96a08016dbc292f039
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-23 17:19:08 -0500

    added a menu and a login button to the header component

--- src/bucket/Header.js
+++ src/bucket/Header.js
@@ -2,32 +2,52 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  toolbar: {
-    textAlign: 'center',
-    height: 80,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  // toolbar: {
+  //   textAlign: 'center',
+  //   height: 80,
+  // },
   heading: {
     margin: 'auto',
   },
-});
+}));
 
 const Header = () => {
   const classes = useStyles();
   return (
     <AppBar position='static'>
-      <Toolbar className={classes.Toolbar}>
-        <Typography
-          className={classes.heading}
-          color='secondary'
-          variant='h3'
-          align='center'
-          fontFamily='Arial'
+      <Toolbar>
+        <IconButton
+          edge='start'
+          className={classes.menuButton}
+          color='inherit'
+          aria-label='menu'
         >
-          Bucket
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          className={classes.title}
+          color='secondary'
+          variant='h4'
+          fontFamily='Nunito'
+        >
+          bucket
         </Typography>
+        <Button color='inherit'>Login</Button>
       </Toolbar>
     </AppBar>
   );
