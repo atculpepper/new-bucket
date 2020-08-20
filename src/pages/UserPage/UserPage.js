d0d5770c4797bcd92e3e778bdb756b9742716540
Anne Culpepper <anne.culpepper@gmail.com>
2020-08-20 14:49:20 -0500

    added inline styling to Container in UserPage. Text content still slides under the NavBar when screen is minimized.

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import Button from '@material-ui/core/Button';
+import { Grid, Typography, Container } from '@material-ui/core';
+import { withStyles, createStyles } from '@material-ui/core/styles';
+
+const customStyles = (theme) =>
+  createStyles({
+    container: {
+      position: 'fixed',
+      marginBottom: '10px',
+    },
+  });
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 const UserPage = (props) => (
   <div>
-    <h1 id='welcome'>Welcome, {props.store.user.username}!</h1>
-    <p>Your ID is: {props.store.user.id}</p>
-    <Button color='inherit' />
+    <Container style={{ marginTop: '15vh' }}>
+      <h1 id='welcome'>Welcome, {props.store.user.username}!</h1>
+      <p>Your ID is: {props.store.user.id}</p>
+      <Button color='inherit' />
+    </Container>
   </div>
 );
 
 // this allows us to use <App /> in index.js
-export default connect(mapStoreToProps)(UserPage);
+export default withStyles(customStyles)(connect(mapStoreToProps)(UserPage));
