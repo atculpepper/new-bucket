23171451bdefb54249bb2e5d9c20cf17b41ad1a5
Anne Culpepper <anne.culpepper@gmail.com>
2020-08-23 17:40:52 -0500

    restyled Login component, bringing in Material UI TextField and Buttons. Connected login functionality.

--- src/bucket/Login/Login.js
+++ src/bucket/Login/Login.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+//material ui components
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import { Grid, Box, TextField, Typography } from '@material-ui/core';
+
 class Login extends Component {
   state = {
     username: '',
@@ -32,57 +37,65 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        {this.props.store.errors.loginMessage && (
-          <h2 className='alert' role='alert'>
-            {this.props.store.errors.loginMessage}
-          </h2>
-        )}
-        <form className='formPanel' onSubmit={this.login}>
-          <h1>Login</h1>
-          <div>
-            <label htmlFor='username'>
-              Username:
-              <input
-                type='text'
-                name='username'
-                value={this.state.username}
-                onChange={this.handleInputChangeFor('username')}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor='password'>
-              Password:
-              <input
-                type='password'
-                name='password'
-                value={this.state.password}
-                onChange={this.handleInputChangeFor('password')}
-              />
-            </label>
-          </div>
-          <div>
-            <input
-              className='log-in'
-              type='submit'
-              name='submit'
-              value='Log In'
+      <Container style={{ marginTop: '10vh', marginLeft: '3vw' }}>
+        <Typography variant='h4'>Login</Typography>
+        <Grid
+          container
+          spacing={2}
+          // style={{ marginTop: '10vh', marginLeft: '5vw' }}
+        >
+          {this.props.store.errors.loginMessage && (
+            <h2 className='alert' role='alert'>
+              {this.props.store.errors.loginMessage}
+            </h2>
+          )}
+          <Grid item xs={12} md={6}>
+            <TextField
+              type='text'
+              placeholder='New Title'
+              fullWidth
+              variant='outlined'
+              label='Username'
+              required
+              // onSubmit={this.login}
+              value={this.state.username}
+              onChange={this.handleInputChangeFor('username')}
             />
-          </div>
-        </form>
-        <center>
-          <button
-            type='button'
-            className='link-button'
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              type='text'
+              placeholder='Password'
+              fullWidth
+              variant='outlined'
+              label='Password'
+              required
+              // onSubmit={this.login}
+              value={this.state.password}
+              onChange={this.handleInputChangeFor('password')}
+            />
+          </Grid>
+
+          <Button
+            variant='outlined'
+            onClick={this.login}
+            style={{ margin: '1vh' }}
+          >
+            Login
+          </Button>
+
+          <Button
+            variant='outlined'
+            // className='link-button'
             onClick={() => {
               this.props.dispatch({ type: 'SET_TO_REGISTER_MODE' });
             }}
+            style={{ margin: '1vh' }}
           >
             Register
-          </button>
-        </center>
-      </div>
+          </Button>
+        </Grid>
+      </Container>
     );
   }
 }
